93eb230b22fabd6ed75519af7856f444e758d810
Steven Accardo <accardo.steven@gmail.com>
2018-03-03 13:49:59 -0800

    First commit, Introduction to creating HOC in React. Created a HOC that checks whether user is logged in, and routes them accordingly.

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      Super Special Secret Recipe
+      <ul>
+        <li>1 Cup Sugar</li>
+        <li>3 Packets of Kool-Aid</li>
+        <li>1 Gallon of Water</li>
+      </ul>
+    </div>
+  );
+};
